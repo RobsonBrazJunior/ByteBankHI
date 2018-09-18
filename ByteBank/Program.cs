204c172c602241378e4c15a3dd0b60aa66fedaae
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-18 10:54:57 -0400

    Criando a classe Funcionario e o método GetBonificação

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ByteBank.Funcionarios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,16 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            Funcionario funcionario = new Funcionario();
+
+            funcionario.Nome = "Nome do funcionario";
+            funcionario.CPF = "CPF do funcionario";
+            funcionario.Salario = 5000;
+
+            Console.WriteLine(funcionario.Nome);
+            Console.WriteLine(funcionario.GetBonificacao());
+
+            Console.ReadLine();
         }
     }
 }
