a59e85352c6c940cce1c7ee6e5f04f5d81106ed0
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-19 14:07:19 -0400

    Aplicando Polimorfismo.

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -11,14 +11,30 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
             Funcionario funcionario = new Funcionario();
 
             funcionario.Nome = "Nome do funcionario";
             funcionario.CPF = "CPF do funcionario";
-            funcionario.Salario = 5000;
+            funcionario.Salario = 2000;
+
+            gerenciador.Registrar(funcionario);
 
             Console.WriteLine(funcionario.Nome);
             Console.WriteLine(funcionario.GetBonificacao());
+
+            Diretor diretor = new Diretor();
+            diretor.Nome = "Nome do diretor";
+            diretor.CPF = "CPF do diretor";
+            diretor.Salario = 5000;
+
+            gerenciador.Registrar(diretor);
+
+            Console.WriteLine(diretor.Nome);
+            Console.WriteLine(diretor.GetBonificacao());
+
+            Console.WriteLine("Total de bonificações: " + gerenciador.GetTotalBonificacao());
 
             Console.ReadLine();
         }
